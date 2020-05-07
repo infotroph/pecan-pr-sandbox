ca2d4fb397749e730d63012402ba668f7dbabf9a
Rahul Agrawal <41531498+rahul799@users.noreply.github.com>
2020-05-07 20:27:49 +0530

    Update model2netcdf.MAAT.R

--- models/maat/R/model2netcdf.MAAT.R
+++ models/maat/R/model2netcdf.MAAT.R
@@ -195,7 +195,7 @@ model2netcdf.MAAT <- function(rundir, outdir, sitelat = -999, sitelon = -999, st
     }
     
     ## extract variable and long names to VAR file for PEcAn vis
-    utils::write.table(sapply(ncout$var, function(x) { x$longname }), 
+         utils::write.table(sapply(ncout$var, function(x) { x$longname }), 
                 file = file.path(outdir, paste(year, "nc.var", sep = ".")), 
                 col.names = FALSE, 
                 row.names = TRUE, 
