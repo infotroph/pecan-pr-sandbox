c03102be6e70f1fc3b76f3dc7bae65c18da94c39
runner <runner@Mac-1481.local>
2020-06-08 14:29:29 +0000

    automated syle update

--- models/fates/R/recurse.create.R
+++ models/fates/R/recurse.create.R
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------------
 # Copyright (c) 2016 NCSA.
 # All rights reserved. This program and the accompanying materials
-# are made available under the terms of the 
+# are made available under the terms of the
 # University of Illinois/NCSA Open Source License
 # which accompanies this distribution, and is available at
 # http://opensource.ncsa.illinois.edu/license.html
@@ -10,7 +10,7 @@
 ##' @name recurse.create
 ##' @title recurse.create
 ##' @description recursively follow the file structure in 'ins' and create all the same folders in 'path' as well as symbolic links to all the file. This is done, rather than creating a symbolic link to the whole structure, so individual files can later be unlinked and replaced with different files/links.
-##' @author Mike   Dietze 
+##' @author Mike   Dietze
 ##' @param path new location to create folders and links
 ##' @param ins  reference lo  cation of inputs
 ##' @export
@@ -19,19 +19,19 @@ recurse.create <- function(path, ins) {
   if (length(files) == 0) {
     return()
   }
-  is.dir <- dir.exists(files)  ## determine which files are actually folders
+  is.dir <- dir.exists(files) ## determine which files are actually folders
   ## create links to true files
   curr <- files[!is.dir]
   for (i in seq_along(curr)) {
     file.symlink(curr[i], file.path(path, basename(curr[i])))
   }
-  
+
   down <- files[is.dir]
   for (i in seq_along(down)) {
     ndir <- file.path(path, basename(down[i]))
     ## create folders
     dir.create(ndir)
-    
+
     ## recurse to fill folders
     recurse.create(ndir, down[i])
   }
